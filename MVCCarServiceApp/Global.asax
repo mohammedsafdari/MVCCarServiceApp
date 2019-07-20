8f5df4e6b4cd99a56e2e8eb04255d99f69bae577
Mohammed Safdari <mohammed.safdari@zensar.com>
2019-07-20 10:51:18 +0530

    First Commit

---
+++ MVCCarServiceApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCCarServiceApp.MvcApplication" Language="C#" %>
---
+++ MVCCarServiceApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCCarServiceApp.Startup))]
+namespace MVCCarServiceApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
